ce11d90dde156ad25ff9408e206641c22da16577
Andrew Maule <developer@bliptrip.net>
2021-03-27 14:54:50 -0500

    Use apt to install pandoc.

--- setup.sh
+++ setup.sh
@@ -2,12 +2,9 @@
 set -e
 set -u
 
-apt-get update
-apt-get install screen
-
-wget https://github.com/jgm/pandoc/releases/download/2.10.1/pandoc-2.10.1-1-amd64.deb
-dpkg -i pandoc-*.deb
-rm -rf pandoc-*.deb
+apt update
+apt upgrade --yes
+apt install screen pandoc --yes
 
 pandoc --version
 
@@ -15,6 +12,3 @@ cd /usr/src/redmine
 
 # Enable HTML in redmine
 sed -iE 's/filter_html\s*=>\s*true/filter_html => false/' "lib/redmine/wiki_formatting/markdown/formatter.rb"
-
-echo "Restart redmine! We enabled HTML in markdown!"
-echo "Use 'screen -S session1' for creating a screen session!"
