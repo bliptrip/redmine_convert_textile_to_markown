16cedac94d6ccf69cfd5669ea7316973e9997d6a
Andrew Maule <developer@bliptrip.net>
2021-03-27 15:18:24 -0500

    Install conversion task rakefile and execute the task.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,9 @@ cd /usr/src/redmine
 
 # Enable HTML in redmine
 sed -iE 's/filter_html\s*=>\s*true/filter_html => false/' "lib/redmine/wiki_formatting/markdown/formatter.rb"
+
+# Copy conversion rake file script
+cp ~/redmine_convert_textile_to_markown/convert_textile_to_markdown.rake lib/tasks/
+
+# Run the conversion
+bundle exec rake convert_textile_to_markdown RAILS_ENV=production
